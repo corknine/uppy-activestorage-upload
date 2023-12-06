d84c659d65d360e6d5a132a79e69b88eca860944
Sebastjan Stojnsek <stirkac@users.noreply.github.com>
2023-12-06 10:43:36 +0100

    Add ability to pass authorization string to header

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,6 +4,7 @@ export interface ActiveStorageUploadOptions extends PluginOptions {
   limit: string;
   bundle: boolean;
   directUploadUrl: string;
+  authorization: string;
 }
 
 export default class ActiveStorageUpload extends Plugin {
